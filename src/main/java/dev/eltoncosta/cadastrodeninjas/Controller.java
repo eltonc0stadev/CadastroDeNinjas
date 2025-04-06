d16e149c2c67b807ba98921822fc1f86c08ebae3
Elton Alcantara <eltonalcancosta@gmail.com>
2025-04-06 10:13:34 -0300

    feat: Criado o ´Controller´

---
+++ src/main/java/dev/eltoncosta/cadastrodeninjas/Controller.java
@@ -0,0 +1,21 @@
+package dev.eltoncosta.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/")
+    public String inicio(){
+        return "Inicio";
+    }
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Boas Vindas";
+    }
+
+}
