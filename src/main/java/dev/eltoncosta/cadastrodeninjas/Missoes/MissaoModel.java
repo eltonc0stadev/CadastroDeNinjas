5038467f655fa6604309fb4257784d359c825a4c
Elton Alcantara <eltonalcancosta@gmail.com>
2025-04-06 21:40:23 -0300

    feat: Coloca o lombok nas classes para abstracoes

--- src/main/java/dev/eltoncosta/cadastrodeninjas/Missoes/MissaoModel.java
+++ src/main/java/dev/eltoncosta/cadastrodeninjas/Missoes/MissaoModel.java
@@ -2,11 +2,15 @@ package dev.eltoncosta.cadastrodeninjas.Missoes;
 
 import dev.eltoncosta.cadastrodeninjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissaoModel {
 
     //niveis: S -> 1, A -> 2, B -> 3, C -> 4
@@ -20,49 +24,4 @@ public class MissaoModel {
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninjas;
 
-    public MissaoModel() {
-    }
-
-    public MissaoModel(Long id, String nome, String descricao, Byte nivel) {
-        this.id = id;
-        this.descricao = descricao;
-        this.nivel = nivel;
-    }
-
-    public MissaoModel(Long id, String nome, String descricao, Byte nivel, List<NinjaModel> ninjas) {
-        this(id, nome, descricao, nivel);
-        this.ninjas = ninjas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Byte getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(Byte nivel) {
-        this.nivel = nivel;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
--- src/main/java/dev/eltoncosta/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/eltoncosta/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -2,11 +2,13 @@ package dev.eltoncosta.cadastrodeninjas.Ninjas;
 
 import dev.eltoncosta.cadastrodeninjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -19,41 +21,4 @@ public class NinjaModel {
     @JoinColumn(name = "missao_id")
     private MissaoModel missao;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel(String nome, String email, int idade, MissaoModel missao) {
-        this(nome, email, idade);
-        this.missao = missao;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
